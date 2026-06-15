4ac15797ce62875f715ff706bc8fe556a9561a7a
Jakub Stęchły <273993729+j-stechly@users.noreply.github.com>
2026-06-15 22:17:03 +0200

    Dodanie możliwości włączenia i wyłączenia ograniczeń zakresu dat dla wykresu.

--- src/widgets/graph.py
+++ src/widgets/graph.py
@@ -1,12 +1,9 @@
-from datetime import datetime
-
 from PyQt6.QtCore import QDate
 from PyQt6.QtWidgets import QWidget
 from PyQt6.QtGui import QPalette, QFont
 
 import pyqtgraph as pg
 
-from src import measurments
 from src.measurments import SugarMeasurementsStore
 from ui.graph_ui import Ui_graph
 
@@ -50,7 +47,7 @@ class Graph(QWidget):
 
         # Update ranges and plot
         self.update_date_ranges(reset_values=True)
-        self.update_range_button_state()
+        self.update_range_reset_button_state()
         self.update_plot()
 
         # Connect QSignal's
@@ -58,13 +55,15 @@ class Graph(QWidget):
         self.ui.from_dateEdit.dateChanged.connect(lambda date: self.from_date_edit_date_changed(date))
         self.ui.to_dateEdit.dateChanged.connect(lambda date: self.to_date_edit_date_changed(date))
         self.ui.date_reset_button.clicked.connect(lambda: self.reset_date_ranges())
+        self.ui.from_checkbox.clicked.connect(lambda checked: self.from_date_checkbox_checked(checked))
+        self.ui.to_checkbox.clicked.connect(lambda checked: self.to_date_checkbox_checked(checked))
 
     def update_date_ranges(self, reset_values:bool = False) -> None:
         """
-        Updates dateEdits allowed date ranges.
-        Optionally reset value, so the cover the whole range
+            Updates dateEdits allowed date ranges.
+            Optionally reset value, so graph covers the whole range
 
-        :param reset_values: If True reset the values
+            :param reset_values: If True reset the values
         """
         dates = [m.when for m in self.store.measurements]
         if len(dates) == 0:
@@ -80,23 +79,30 @@ class Graph(QWidget):
             self.reset_date_ranges()
 
     def reset_date_ranges(self) -> None:
-        """Resets dateEdits date ranges, so they cover whole the whole range"""
-        date_min = self.ui.from_dateEdit.minimumDate()
-        date_max = self.ui.to_dateEdit.maximumDate()
+        """Resets enabled dateEdits date ranges, so graph covers the whole range"""
+        if self.ui.from_checkbox.isChecked():
+            date_min = self.ui.from_dateEdit.minimumDate()
+            self.ui.from_dateEdit.setDate(date_min)
 
-        self.ui.from_dateEdit.setDate(date_min)
-        self.ui.to_dateEdit.setDate(date_max)
+        if self.ui.to_checkbox.isChecked():
+            date_max = self.ui.to_dateEdit.maximumDate()
+            self.ui.to_dateEdit.setDate(date_max)
 
-    def update_range_button_state(self) -> None:
+    def update_range_reset_button_state(self) -> None:
         """
-        Enables button if range reset button if full range is not covered.
-        Disabled if it is.
+        Enables range reset button, if range covered under enabled dateEdits is not equal to whole available range.
+        Disables in other cases.
         """
-        date_min = self.ui.from_dateEdit.minimumDate()
-        date_max = self.ui.to_dateEdit.maximumDate()
+        enable = False
+        if self.ui.from_checkbox.isChecked():
+            date_min = self.ui.from_dateEdit.minimumDate()
+            enable = enable or date_min != self.ui.from_dateEdit.date()
 
-        disabled = date_min == self.ui.from_dateEdit.date() and date_max == self.ui.to_dateEdit.date()
-        self.ui.date_reset_button.setDisabled(disabled)
+        if self.ui.to_checkbox.isChecked():
+            date_max = self.ui.to_dateEdit.maximumDate()
+            enable = enable or date_max != self.ui.to_dateEdit.date()
+
+        self.ui.date_reset_button.setEnabled(enable)
 
     def update_plot(self) -> None:
         """Updates plot"""
@@ -107,6 +113,13 @@ class Graph(QWidget):
         measurements.sort(key=lambda x: x.when)
         x_data = []
         y_data = []
+
+        if not self.ui.from_checkbox.isChecked():
+            from_date = min(measurements, key=lambda x: x.when).when.date()
+
+        if not self.ui.to_checkbox.isChecked():
+            to_date = max(measurements, key=lambda x: x.when).when.date()
+
         for m in measurements:
             if from_date <= m.when.date() <= to_date:
                 x_data.append(m.when.timestamp())
@@ -121,15 +134,25 @@ class Graph(QWidget):
 
     def measurements_changed(self) -> None:
         self.update_date_ranges()
-        self.update_range_button_state()
+        self.update_range_reset_button_state()
         self.update_plot()
 
     def from_date_edit_date_changed(self, date: QDate) -> None:
         self.ui.to_dateEdit.setMinimumDate(date)
-        self.update_range_button_state()
+        self.update_range_reset_button_state()
         self.update_plot()
 
     def to_date_edit_date_changed(self, date: QDate) -> None:
         self.ui.from_dateEdit.setMaximumDate(date)
-        self.update_range_button_state()
+        self.update_range_reset_button_state()
+        self.update_plot()
+
+    def from_date_checkbox_checked(self, checked: bool):
+        self.ui.from_dateEdit.setEnabled(checked)
+        self.update_range_reset_button_state()
+        self.update_plot()
+
+    def to_date_checkbox_checked(self, checked: bool):
+        self.ui.to_dateEdit.setEnabled(checked)
+        self.update_range_reset_button_state()
         self.update_plot()
